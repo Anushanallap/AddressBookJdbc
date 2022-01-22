7a3501ebe5d1a199c9d8818110c0758be572d9de
Anushanallap <94031566+Anushanallap@users.noreply.github.com>
2022-01-22 11:37:21 +0530

    Update AddressBookTest.java

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -13,35 +13,39 @@ public class AddressBookTest {
     @Test
     public void retrieveAlltheEntriesInAddressBookTest()
     {
+        AddressBookDB addressbookdb = new AddressBookDB();
         List<AddressBook> addresstest = AddressBookDB.retrieveAlltheEntriesInAddressBook();
-        Assertions Assert = null;
-        Assert.assertEquals(8, addresstest.size());
+                Assertions.assertEquals(3,addressbooktest.size());
     }
     @Test
     public void updateContactInfoTest() {
+        
+        AddressBookDB addressbookdb = new AddressBookDB();
+        List<AddressBook> addresstest = AddressBookDB.updatecontactinfotetsInAddressBook();
         int contact = AddressBookDB.updateContactInfo("makreting","Anusha","Nallapu");
-        Assertions Assert = null;
-        Assert.assertEquals(1, contact);
+       Assertions.assertEquals(1, contact());
     }
     @Test
     public void retrieveContactFromDatabaseTest() {
+        
+        AddressBookDB addressbookdb = new AddressBookDB();
         LocalDate start_date = LocalDate.of(2021,10,02);
         LocalDate end_date = LocalDate.of(2022,11,05);
         List<AddressBook> dateresult = AddressBookDB.retrieveContactFromDatabase(start_date,end_date);
-        Assertions Assert = null;
-        Assert.assertEquals(3, dateresult.size());
+        Assertions.assertEquals(2, Dateresult.size());
     }
     @Test
     public void retrieveNumberOfContactsInDbByCityorStateTest() throws SQLException {
-        List<AddressBook> cityorstatetest = AddressBookDB.retrieveContactsByCityorState("kolar");
-        Assertions Assert = null;
-        Assert.assertEquals(1, cityorstatetest.size());
+        AddressBookDB addressbookdb = new AddressBookDB();
+        List<AddressBook> cityorstatetest = AddressBookDB.retrieveContactsByCityorState("karnataka");
+          Assertions.assertEquals(1, cityorstatetest.size());
     }
+
     @Test
     public void addDataIntoAddressBookTransactionTest() throws SQLException {
+         AddressBookDB addressbookdb = new AddressBookDB();
         LocalDate start_date = LocalDate.of(2019,03, 01);
         int result = AddressBookDB.addDatabaseIntoTransaction("bhavitha","potha","sathupally","sales",start_date, "kallur","Telangana",507303,987586412,"bhavi@gmail.com");
-        Assertions Assert = null;
-        Assert.assertEquals(1, result);
+        Assertions.assertEquals(1, result);
     }
 }
